31d9adad3111f9ada76c2d5d01a3759060794adf
Eric <eric.trignau@gmail.com>
2026-02-06 14:25:32 +0100

    navbar ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import Feed from "./pages/Feed/Feed";
+import Messages from "./pages/Messages/Messages";
+import Profile from "./pages/Profile/Profile";
+import Layout from "./components/Layout";
 
 function App() 
 {
   return (
     <BrowserRouter>
       <Routes>
+		{/*ROUTE SANS NAVBAR */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+
+		{/*ROUTE AVEC NAVBAR*/}
+		<Route path="/feed" element={<Layout />} />
+		<Route path="/messages" element={<Layout />} />
+		<Route path="/profile" element={<Layout />} />
+
+		{/*REDIRECTION PAR DEFAUT*/}
         <Route path="/" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
---
+++ frontend/src/pages/Profile/Profile.tsx
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Profile.tsx                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eric <eric@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/06 14:07:20 by eric              #+#    #+#             */
+/*   Updated: 2026/02/06 14:07:24 by eric             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+export default function Profile() 
+{
+    return (
+        <div>
+            <h1 className="text-3xl font-bold mb-6">Profile</h1>
+            <p className="text-gray-600">Page de profil (à coder)</p>
+        </div>
+    );
+}
